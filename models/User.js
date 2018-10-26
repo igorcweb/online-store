57c2f733f96a9dfbd1f12eab86f297731de353fb
Igor Chernyshev <igorchern07@gmail.com>
2018-10-26 13:58:10 -0500

    User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+
+  prime: {
+    type: Boolean,
+    default: false
+  },
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ]
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
