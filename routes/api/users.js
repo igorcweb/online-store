906250877deb04e7fa54b52fa2e18a6d41b113cb
Igor Chernyshev <igorchern07@gmail.com>
2018-10-26 15:21:57 -0500

    adding new users to database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route GET api/users/test
 // @access Public
 router.get('/test', (req, res) => res.json({ message: 'users route works' }));
+
+// @route GET api/users/register
+// @access Public
+router.post('/register', (req, res) => {
+  //Check if user exists
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const newUser = new User(req.body);
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
