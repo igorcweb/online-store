ef58467b9a64ff21929550f93a2fd1ba1e1fbc37
Igor Chernyshev <igorchern07@gmail.com>
2018-11-16 17:51:23 -0600

    client/build

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+} else {
+  app.use(express.static('client/public'));
+}
 
 // DB Config
 const db = process.env.MONGODB_URI || 'mongodb://localhost/online-store';
