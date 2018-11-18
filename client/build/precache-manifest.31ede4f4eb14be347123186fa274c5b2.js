43807b6ea73b7fcad61ee2078286bf5451710644
Igor Chernyshev <igorchern07@gmail.com>
2018-11-18 17:44:23 -0600

    build

---
+++ client/build/precache-manifest.31ede4f4eb14be347123186fa274c5b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b9a5cd1abdf533be8bf4a1af84fa3ee",
+    "url": "/static/media/spinner.8b9a5cd1.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82fb7ce9cf7e3723648a",
+    "url": "/static/js/main.82fb7ce9.chunk.js"
+  },
+  {
+    "revision": "e82c009bbf2fe76d6155",
+    "url": "/static/js/1.e82c009b.chunk.js"
+  },
+  {
+    "revision": "82fb7ce9cf7e3723648a",
+    "url": "/static/css/main.d3889048.chunk.css"
+  },
+  {
+    "revision": "72de534c51e074e3420ec44bc7ed1934",
+    "url": "/index.html"
+  }
+];
