ec01f8f3d4f1d82b65423d92cfb2bdf47f25b7ff
Igor Chernyshev <igorchern07@gmail.com>
2018-11-19 23:50:09 -0600

    Update send.js

--- routes/api/send.js
+++ routes/api/send.js
@@ -104,7 +104,7 @@ router.post('/order', (req, res, next) => {
     const errorEmails = [];
     const successfulEmails = [];
     const sg = require('sendgrid')(
-      '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'
+      process.env.SENDGRID_API_KEY
     );
     async.parallel(
       [
