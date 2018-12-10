2cfd1c41cab28cafb6cd021cfaadb10c1b14e7d1
Igor Chernyshev <igorchern07@gmail.com>
2018-12-09 23:59:14 -0600

    landing file structure

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import Landing from './components/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/Dashboard';
